e124262a9194b2e6388975b07e19cd6eeee24024
Lorena Galeano <lorenagaleano@Lorenas-MBP-2.home>
2021-11-01 15:34:33 -0400

    tens.html and tens.py

---
+++ tens.py
@@ -0,0 +1,9 @@
+# this is for java
+#for (var i = 20; i <= 100; i = i +10){
+#        console.log(i);
+#}
+
+for i in range(20, 110, 10):
+    print(i)
+
+    #range(<inital value>,) <termination conditon>, <increment>)
